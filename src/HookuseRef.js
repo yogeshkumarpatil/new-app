a327ac4b3949e4f68ea99e578eca84e93c6dd385
yogeshkumarpatil <appseeds.yogeshkumar@gmail.com>
2023-04-27 14:41:51 +0530

    useRef in Functional componet 1st Part

---
+++ src/HookuseRef.js
@@ -0,0 +1,45 @@
+import React, {useEffect, useRef} from "react";
+
+function HookuseRef() {
+    // const newRef = useRef();
+    const firstRef = useRef();
+    const lastRef = useRef();
+
+    useEffect(() => {
+        // console.log(newRef);
+        console.log(firstRef);
+        console.log(lastRef); 
+        firstRef.current.focus();       
+        firstRef.current.style.color = 'red';
+        firstRef.current.style.backgroundColor = 'yellowdsrfwswf';
+        // console.log(newRef.current.innerHTML);
+    });
+
+    // const handleHeading =() =>{
+    //     newRef.current.style.color = 'red';
+    //     newRef.current.style.fontFamily = 'cooper';
+    //     newRef.current.hidden = true;
+    // }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log('Full Name = ', firstRef.current.value, lastRef.current.value);
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor=""> Enter First Name : </label>
+                <input type="text" ref={firstRef} placeholder="First Name" />
+                <br /> <br />
+                <label htmlFor=""> Enter Last Name : </label>
+                <input type="text" ref={lastRef} placeholder="Last Name" />
+                <br /> <br />
+                <input type="submit" value="Submit" />
+            </form>
+            {/* <h1 ref={newRef}>Yogesh Sanlig Maharashtra</h1>
+      <button onClick={handleHeading}>Changer</button> */}
+        </div>
+    );
+}
+
+export default HookuseRef;
